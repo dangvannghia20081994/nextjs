c617775e3b66c2594e71201916d39335302e5642
Đặng Văn Nghĩa <nghiadv@elcom.com.vn>
2022-05-12 15:53:00 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,45 +1,52 @@
-import { DefaultSeo } from 'next-seo'
-import SEO from 'next-seo.config'
-import { Provider as AppProvider } from 'react-redux'
-import { useEffect } from 'react'
+import { DefaultSeo } from "next-seo";
+import SEO from "next-seo.config";
+import { Provider as AppProvider } from "react-redux";
+import { useEffect } from "react";
 import { MathJaxContext } from "better-react-mathjax";
-import SSRProvider from 'react-bootstrap/SSRProvider';
+import SSRProvider from "react-bootstrap/SSRProvider";
 
-
-import Default from '~/layouts/default'
-import { getData } from '~/utils/request'
+import Default from "~/layouts/default";
+import { getData } from "~/utils/request";
 import store, { setUser, setClasses, setSubjects, setBanners } from "~/store";
-import { accessToken } from '~/common/hook/useToken'
-import '~/styles/modal/Login.scss'
-import '~/styles/globals.scss'
+import { accessToken } from "~/common/hook/useToken";
+import "~/styles/modal/Login.scss";
+import "~/styles/globals.scss";
 
-import Router from 'next/router';
-import NProgress from 'nprogress'; //nprogress module
-import 'nprogress/nprogress.css'; //styles of nprogress
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
 import "moment/locale/vi";
 
-const MyApp = ({ Component, pageProps, user, classes, subjects, banners, ...props }) => {
+const MyApp = ({
+  Component,
+  pageProps,
+  user,
+  classes,
+  subjects,
+  banners,
+  ...props
+}) => {
   useEffect(() => {
     //Binding events.
     Router.events.on("routeChangeStart", () => NProgress.start());
     Router.events.on("routeChangeComplete", () => NProgress.done());
     Router.events.on("routeChangeError", () => NProgress.done());
-    if (user) store.dispatch(setUser(user))
-    if (classes) store.dispatch(setClasses(classes))
-    if (subjects) store.dispatch(setSubjects(subjects))
+    if (user) store.dispatch(setUser(user));
+    if (classes) store.dispatch(setClasses(classes));
+    if (subjects) store.dispatch(setSubjects(subjects));
     if (banners) store.dispatch(setBanners(banners));
     return () => {
       Router.events.off("routeChangeStart", () => NProgress.start());
       Router.events.off("routeChangeComplete", () => NProgress.done());
       Router.events.off("routeChangeError", () => NProgress.done());
-    }
-  }, [])
-  const Layout = Component.Layout || Default
+    };
+  }, []);
+  const Layout = Component.Layout || Default;
 
   const config = {
     options: {
       enableMenu: false,
-    }
+    },
   };
   return (
     <MathJaxContext config={config}>
@@ -52,25 +59,25 @@ const MyApp = ({ Component, pageProps, user, classes, subjects, banners, ...prop
         </AppProvider>
       </SSRProvider>
     </MathJaxContext>
-  )
-}
+  );
+};
 MyApp.getInitialProps = async ({ ctx }) => {
-  let user = null
-  let classes = null
-  let subjects = null
-  let banners = []
+  let user = null;
+  let classes = null;
+  let subjects = null;
+  let banners = [];
   const req = ctx.req;
   if (req) {
-    const cookies = req.cookies
+    const cookies = req.cookies;
     if (cookies[accessToken]) {
-      const { data } = await getData('profile/user', {}, cookies[accessToken])
-      user = data
-      user.token = cookies[accessToken]
+      const { data } = await getData("profile/user", {}, cookies[accessToken]);
+      user = data;
+      user.token = cookies[accessToken];
     }
-    const { data: classes_ } = await getData('category/class')
-    classes = classes_
-    const { data: subjects_ } = await getData('category/subject')
-    subjects = subjects_
+    const { data: classes_ } = await getData("category/class");
+    classes = classes_;
+    const { data: subjects_ } = await getData("category/subject");
+    subjects = subjects_;
     const { data: banners_ } = await getData("config/banner");
     banners = banners_;
   }
@@ -80,5 +87,5 @@ MyApp.getInitialProps = async ({ ctx }) => {
     subjects,
     banners,
   };
-}
-export default MyApp
+};
+export default MyApp;
