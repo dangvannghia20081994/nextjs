854f66a8e6767dff6664c6506a770f357ae58005
Đặng Văn Nghĩa <nghiadv@elcom.com.vn>
2022-06-07 10:18:16 +0700

    update

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,23 +1,17 @@
 import EmptyLayout from '~/layouts/empty'
 import { useState, useEffect, useRef } from 'react'
+import { useRouter } from 'next/router'
 const Test = () => {
-  const [text, setText] = useState('')
+  const router = useRouter()
+  const query = router.query;
+  const [text, setText] = useState()
   const resultRef = useRef()
   const convertHTML = (str) => {
-    //remove attribute
-    document.querySelectorAll("#html-convert *").forEach((element) => {
-      if (element) {
-        element.removeAttribute("cellspacing");
-        element.removeAttribute("cellpadding");
-        element.removeAttribute("id");
-        element.removeAttribute("style");
-        element.removeAttribute("class");
-        element.removeAttribute("rel");
-        element.removeAttribute("dir");
-      }
-    });
+    const tag = document.querySelector("#html-convert");
+    if (!tag) { return }
+    tag.innerHTML = text;
     //remove thead
-    document.querySelector("#html-convert")?.firstChild?.firstChild?.remove()
+    tag?.firstChild?.firstChild?.remove();
     //remove first row
     document.querySelector("#html-convert table tbody tr:first-child")?.remove()
     //remove column first
@@ -31,60 +25,91 @@ const Test = () => {
       }
     });
     //remove column private
-    // [12, 3].forEach((ind) => {
-    //   document.querySelectorAll(`#html-convert table tbody tr td:nth-child(${ind})`).forEach(ele => {
-    //     ele.remove();
-    //   });
-    // })
+    const removes = (query.remove?.replace(/\D/g, "").split('').map(Number) || []).sort().reverse()
+    removes.forEach((ind) => {
+      document.querySelectorAll(`#html-convert table tbody tr td:nth-child(${ind})`).forEach(ele => {
+        ele.remove();
+      });
+    })
+    //remove attribute
+    const attribute = JSON.parse(query.attribute || 'false')
+    if (!attribute) {
+      document.querySelectorAll("#html-convert *").forEach((element) => {
+        if (element) {
+          element.removeAttribute("cellspacing");
+          element.removeAttribute("cellpadding");
+          element.removeAttribute("id");
+          element.removeAttribute("style");
+          element.removeAttribute("class");
+          element.removeAttribute("rel");
+          element.removeAttribute("dir");
+        }
+      });
+    }
     document.querySelector('#html-convert table')?.classList?.add('table','table-bordered','table-dark','table-striped-columns')
+  };
+  const getString = () => {
+    const list = [];
+    const table = document.querySelector("table");
+    if (!table) return;
+    const gets = (query.get?.replace(/\D/g, "").split('').map(Number) || [1, 5, 6]).sort()
+    const trs = table.getElementsByTagName("tr");
+    Array.from(trs).forEach((tr, ind) => {
+      if (ind === 0) {
+        return;
+      }
+      const td = tr.getElementsByTagName("td");
+      let listTmp = []
+      gets.forEach(idx => {
+        if (td[idx]) {
+          let temp = td[idx].textContent || td[idx].innerText;
+          listTmp.push(temp);
+        }
+      })
+      const finall = listTmp.join(' - ').trim();
+      finall = finall.replace(/(ck+ )(\d*)/, "");
+      finall = finall.replace(/%/g, "");
+      finall = finall.replace(/\n/g, " ");
+      finall = finall.replace(/\t/g, "");
+      finall = finall.replace(/(\s\w+\s)(\d*\s)/, "");
+      // finall = finall.replace(/(thưởng )(\d*)/, "");
+      finall = finall.replace(/--*/, "");
+      finall = finall.replace(/\s+/g, " "); // mutiple space to single space
+      finall = finall.trim()
+      list.push(finall);
+    });
+    resultRef.current.value = list.join("\n");
+    localStorage.setItem('rooms', list.join('\n'))
   };
   const filter = (filter = '') => {
     const list = []
     filter = filter?.toUpperCase()
-    var table, trs, td, i, txtValue;
+    var table, trs;
     table = document.querySelector("table");
     if (!table) return;
     trs = table.getElementsByTagName("tr"); 
-    // Loop through all table rows, and hide those who don't match the search query
     Array.from(trs).forEach((tr, ind) => {
-      if (ind === 0) { return }
-      const td = tr.getElementsByTagName('td')
-      const sttAddress = 1
-      const sttPrice = 5
-      const sttType = 6
-      let address = td[sttAddress].textContent || td[sttAddress].innerText;
-      address = address.replace(/ +/g, " ");// mutiple space to single space
-      const arr = address.split('-')
-      if (arr.length === 2) {
-      } else if (arr.length === 3) {
-        address = arr.slice(arr.length - 1, arr.length).join().trim();
-      } else if (arr.length === 4 || arr.length === 5) {
-        address = arr.slice(arr.length - 2, arr.length).join('-').trim();
+      let td = tr.getElementsByTagName("td")[11];
+      if (td) {
+        let txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr.style.display = "";
+        } else {
+          tr.style.display = "none";
+        }
       }
-      address = address.replace(/(ck )([0-9]+)/, "");
-      const price = td[sttPrice].textContent || td[sttPrice].innerText;
-      const type = td[sttType].textContent || td[sttType].innerText;
-      const finall = `${address} - ${price} - ${type}`;
-      finall = finall.replace(/\n/g, ' ');
-      finall = finall.replace(/\t/g, '');
-      finall = finall.replace(/%/g, '');
-      list.push(finall);
-      // td = tr.getElementsByTagName("td")[11];
-      // if (td) {
-      //   txtValue = td.textContent || td.innerText;
-      //   if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      //     tr.style.display = "";
-      //   } else {
-      //     tr.style.display = "none";
-      //   }
-      // }
     })
     resultRef.current.value = list.join('\n')
   };
   useEffect(() => {
+    const rooms = localStorage.getItem("rooms") || '';
+    resultRef.current.value = rooms;
+  }, [])
+  
+  useEffect(() => {
     document.querySelector("#html-convert").innerHTML = text;
     convertHTML(text);
-    filter()
+    getString();
   }, [text])
   
   return (
